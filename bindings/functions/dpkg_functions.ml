889da64b4e678670e559263178a96aef2e99e395
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2019-04-03 13:45:16 +0200

    layout, gitignore and opam file

--- bindings/functions/dpkg_functions.ml
+++ bindings/functions/dpkg_functions.ml
@@ -1,13 +1,26 @@
 open Ctypes
-       
+
 module M (F : Ctypes.FOREIGN) = struct
   open F
   include Dpkg_types.M(Dpkg_types_stubs)
 
-  let dpkg_version_blank = foreign "dpkg_version_blank" (ptr dpkg_version @-> returning void)
-  let dpkg_version_is_informative = foreign "dpkg_version_is_informative" (ptr dpkg_version @-> returning bool)
-  let dpkg_version_compare = foreign "dpkg_version_compare" (ptr dpkg_version @-> ptr dpkg_version @-> returning int)
-let dpkg_version_relate = foreign "dpkg_version_relate" (ptr dpkg_version @-> dpkg_relation @-> ptr dpkg_version @-> returning bool)
-                                     
-  let parseversion = foreign "parseversion" (ptr dpkg_version @-> string @-> ptr dpkg_error @-> returning int)
+  let dpkg_version_blank =
+    foreign "dpkg_version_blank"
+      (ptr dpkg_version @-> returning void)
+
+  let dpkg_version_is_informative =
+    foreign "dpkg_version_is_informative"
+      (ptr dpkg_version @-> returning bool)
+
+  let dpkg_version_compare =
+    foreign "dpkg_version_compare"
+      (ptr dpkg_version @-> ptr dpkg_version @-> returning int)
+
+  let dpkg_version_relate =
+    foreign "dpkg_version_relate"
+      (ptr dpkg_version @-> dpkg_relation @-> ptr dpkg_version @-> returning bool)
+
+  let parseversion =
+    foreign "parseversion"
+      (ptr dpkg_version @-> string @-> ptr dpkg_error @-> returning int)
 end
--- bindings/types/dpkg_types.ml
+++ bindings/types/dpkg_types.ml
@@ -6,20 +6,20 @@ module M (T : Ctypes.TYPE) = struct
     enum name (List.map (fun (a, b) -> (a, constant b int64_t)) vals)
 
   type dpkg_msg_type =
-    DPKG_MSG_NONE | DPKG_MSG_WARN | DPKG_MSG_ERROR
+      DPKG_MSG_NONE | DPKG_MSG_WARN | DPKG_MSG_ERROR
 
   let dpkg_msg_type =
     enum "dpkg_msg_type" [
-        DPKG_MSG_NONE  , "DPKG_MSG_NONE"  ;
-        DPKG_MSG_WARN  , "DPKG_MSG_WARN"  ;
-        DPKG_MSG_ERROR , "DPKG_MSG_ERROR" ;
-      ]
+      DPKG_MSG_NONE  , "DPKG_MSG_NONE"  ;
+      DPKG_MSG_WARN  , "DPKG_MSG_WARN"  ;
+      DPKG_MSG_ERROR , "DPKG_MSG_ERROR" ;
+    ]
 
   type dpkg_error
   let dpkg_error : dpkg_error structure typ = structure "dpkg_error"
   let type_ = field dpkg_error "type" dpkg_msg_type
   let () = seal dpkg_error
-                
+
   type dpkg_version
   let dpkg_version : dpkg_version structure typ = structure "dpkg_version"
   let epoch    = field dpkg_version "epoch"    int
@@ -28,16 +28,16 @@ module M (T : Ctypes.TYPE) = struct
   let () = seal dpkg_version
 
   type dpkg_relation =
-    DPKG_RELATION_NONE | DPKG_RELATION_EQ | DPKG_RELATION_LT
+      DPKG_RELATION_NONE | DPKG_RELATION_EQ | DPKG_RELATION_LT
     | DPKG_RELATION_LE | DPKG_RELATION_GT | DPKG_RELATION_GE
 
   let dpkg_relation =
     enum "dpkg_relation" [
-        DPKG_RELATION_NONE , "DPKG_RELATION_NONE" ;
-        DPKG_RELATION_EQ   , "DPKG_RELATION_EQ"   ;
-        DPKG_RELATION_LT   , "DPKG_RELATION_LT"   ;
-        DPKG_RELATION_LE   , "DPKG_RELATION_LE"   ;
-        DPKG_RELATION_GT   , "DPKG_RELATION_GT"   ;
-        DPKG_RELATION_GE   , "DPKG_RELATION_GE"   ;
-      ]
+      DPKG_RELATION_NONE , "DPKG_RELATION_NONE" ;
+      DPKG_RELATION_EQ   , "DPKG_RELATION_EQ"   ;
+      DPKG_RELATION_LT   , "DPKG_RELATION_LT"   ;
+      DPKG_RELATION_LE   , "DPKG_RELATION_LE"   ;
+      DPKG_RELATION_GT   , "DPKG_RELATION_GT"   ;
+      DPKG_RELATION_GE   , "DPKG_RELATION_GE"   ;
+    ]
 end
